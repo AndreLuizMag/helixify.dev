75df5264413cdecc6f3008a09be4c14a045f9e87
André Luiz <andreluizmagjr@outlook.com>
2024-04-02 00:24:49 -0300

    Ajust for build

--- src/components/Helixify/ContentWrapper/index.tsx
+++ src/components/Helixify/ContentWrapper/index.tsx
@@ -44,8 +44,8 @@ export const ContentWrapper = () => {
 			<SecIntro.Subtitle>
 				Start your projects with ease and efficiency using
 				the powerful open-source tools provided by Helixify.
-				Here, you'll find the foundation you need to bring
-				your ideas to life quickly and optimally.
+				Here, you&apos;ll find the foundation you need to
+				bring your ideas to life quickly and optimally.
 			</SecIntro.Subtitle>
 		</SecIntro.Root>,
 	]
