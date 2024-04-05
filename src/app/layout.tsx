48338b6b33d56c79f29d51f46011222850588944
andre.aguiar <andre.aguiar@bevicred.com.br>
2024-04-05 09:57:21 -0300

    Add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import '@/styles/globals.scss'
 
 import { Header } from '@/components/Header'
@@ -22,6 +23,7 @@ export default function RootLayout({
 				{children}
 				<Footer />
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	)
