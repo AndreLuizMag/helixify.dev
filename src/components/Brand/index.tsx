d20318d92fb6ebd5cd588469398cb4bb9365092c
André Luiz <andreluizmagjr@outlook.com>
2024-04-06 18:25:23 -0300

    Add footer

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -2,10 +2,12 @@ import { Helixify } from './Helixify'
 import { HelixCSS } from './HelixCSS'
 import { HelixColor } from './HelixColor'
 import { Icon } from './Icon'
+import { Text } from './Text'
 
 export const Brand = {
 	Helixify,
 	HelixCSS,
 	HelixColor,
 	Icon,
+	Text,
 }
