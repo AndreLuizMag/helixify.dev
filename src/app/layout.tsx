51cd12534307d83a7aa460e2602609977a7c18ca
andre.aguiar <andre.aguiar@bevicred.com.br>
2024-04-10 17:31:24 -0300

    Test build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
+import '@/styles/globals.scss'
 
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
-
-import '../styles/globals.scss'
 
 export const metadata: Metadata = {
 	title: 'Helixify',
@@ -22,17 +21,6 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<head>
-				<link rel='icon' href='/favicon.ico' sizes='any' />
-				{/* <link
-					rel='stylesheet'
-					href='https://cdn.jsdelivr.net/gh/Helixify/HelixCSS@latest/dist/compressed/main.css'
-				/>
-				<link
-					rel='stylesheet'
-					href='https://cdn.jsdelivr.net/gh/Helixify/HelixColor@latest/dist/compressed/color-scheme.css'
-				/> */}
-			</head>
 			<body className='theme-primary ds-flex flow-col-nw justify-between'>
 				<div className='width-100 ds-flex flow-col-nw'>
 					<Header />
