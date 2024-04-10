03d45b2eaca3ca7cbce44147d2eb31de1f44dec4
andre.aguiar <andre.aguiar@bevicred.com.br>
2024-04-10 16:41:49 -0300

    Update content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import { HelixifyWrapped } from '@/templates/Helixify'
+import type { Metadata } from 'next'
 import '@/styles/helixify.scss'
+import { HelixifyWrapped } from '@/templates/Helixify'
+
+export const metadata: Metadata = {
+	title: 'Helixify',
+	description:
+		'platform for provides open-source solutions',
+}
 
 const Home = () => {
 	return (
