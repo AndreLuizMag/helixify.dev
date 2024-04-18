7d0ad73671f30482f25ca934c71f3befa89a57cd
andre.aguiar <andre.aguiar@bevicred.com.br>
2024-04-18 10:29:40 -0300

    Add Font Family Polly Rounded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,33 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
 import '@/styles/main.scss'
+// import localFont from 'next/font/local'
+
+// const PollyRounded = localFont({
+// 	src: [
+// 		{
+// 			path: '../fonts/PollyRounded/polly-rounded-thin.otf',
+// 			weight: '100',
+// 			style: 'normal',
+// 		},
+// 		{
+// 			path: '../fonts/PollyRounded/polly-rounded-light.otf',
+// 			weight: '200',
+// 			style: 'normal',
+// 		},
+// 		{
+// 			path: '../fonts/PollyRounded/polly-rounded-regular.otf',
+// 			weight: '400',
+// 			style: 'normal',
+// 		},
+// 		{
+// 			path: '../fonts/PollyRounded/polly-rounded-bold.otf',
+// 			weight: '700',
+// 			style: 'normal',
+// 		},
+// 	],
+// 	display: 'swap',
+// })
 
 export const metadata: Metadata = {
 	title: 'Helixify',
