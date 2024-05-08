1f067cef66e52e7cd779d33853180cf9e1100a06
André Luiz <andreluizmagjr@outlook.com>
2024-05-07 22:36:51 -0300

    Update content

--- src/templates/Helixify/Overall/Card/Button.tsx
+++ src/templates/Helixify/Overall/Card/Button.tsx
@@ -1,5 +1,6 @@
 import React, { FC, HTMLAttributes, ReactNode } from 'react'
 import Link from 'next/link'
+import { CaretRight } from '@phosphor-icons/react'
 
 interface ButtonProps extends HTMLAttributes<HTMLElement> {
 	href: string
@@ -10,5 +11,12 @@ export const Button: FC<ButtonProps> = ({
 	href,
 	children,
 }) => {
-	return <Link href={href}>{children}</Link>
+	return (
+		<Link
+			href={href}
+			className='ds-inline-flex flow-row-nw align-end gap-sm text-decoration-none font-tertiary'>
+			<span>{children}</span>
+			<CaretRight size={16} className='mb-1' />
+		</Link>
+	)
 }
--- src/templates/Helixify/Overall/Card/Root.tsx
+++ src/templates/Helixify/Overall/Card/Root.tsx
@@ -15,9 +15,9 @@ const Root: FC<RootProps> = ({ variant, children }) => {
 	return (
 		<VariantContext.Provider value={variant}>
 			<div
-				className={`overall-card ds-flex flow-col-nw justify-center align-start flex-bgs gap-lg p-${
+				className={`overall-card overall-card-${variant} ps-relative ds-flex flow-col-nw justify-center align-start flex-bgs gap-lg p-${
 					variant === 'max' ? '16' : '10'
-				} lg:p-10 radius-md theme-secondary`}>
+				} lg:p-10 overflow-hidden radius-md theme-secondary`}>
 				{children}
 			</div>
 		</VariantContext.Provider>
